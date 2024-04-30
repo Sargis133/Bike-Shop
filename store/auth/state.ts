1902fab6edcddd7676671ed8e85c240f8df96ca7
Sargis <saqobabloyan@gmail.com>
2024-04-30 16:50:02 +0400

    change auth config

--- store/auth/state.ts
+++ store/auth/state.ts
@@ -4,8 +4,8 @@ import {I_AuthState} from "~/models/store/auth/interfaces";
 
 const state: I_AuthState = {
     users: [
-        {name: 'Marko', email: 'markomiller@mail.ru', phone: '123123', password: 'lolik1.', confirmPassword: 'lolik1.'},
-        {name: 'Semi', email: 'miller@mail.ru', phone: '123123', password: 'lolik2.', confirmPassword: 'lolik2.'}
+        {name: 'Marko', email: 'markomiller@mail.ru', phone: '123123', password: 'password.1', confirmPassword: 'lolik1.'},
+        {name: 'Semi', email: 'miller@mail.ru', phone: '123123', password: 'password.2', confirmPassword: 'lolik2.'}
     ],
     userToken: null
 }
