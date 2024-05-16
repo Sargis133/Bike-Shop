4d396f869233ba9a6c9d0f0958ad2e0c12ac14ba
Sargis <saqobabloyan@gmail.com>
2024-05-16 09:33:33 +0400

    change user password

--- store/auth/state.ts
+++ store/auth/state.ts
@@ -4,7 +4,7 @@ import {I_AuthState} from "~/models/store/auth/interfaces";
 
 const state: I_AuthState = {
     users: [
-        {name: 'User', email: 'user@mail.ru', phone: '123123', password: 'password1.', confirmPassword: 'password'},
+        {name: 'User', email: 'user', phone: '123123', password: 'password', confirmPassword: 'password'},
     ],
     userToken: null
 }
